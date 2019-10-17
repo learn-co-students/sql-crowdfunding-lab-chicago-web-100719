68f7338c41c6b0f39efa240d8c5632a453d806fa
NaveedG <nganjani@protonmail.com>
2019-10-17 07:59:44 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,81 @@
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+INSERT INTO users(name, age) VALUES ('Zayd', 33);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES ('project', 'tech', 100000, 2019, 2020);
+
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
+INSERT INTO pledges (amount) VALUES (1000);
